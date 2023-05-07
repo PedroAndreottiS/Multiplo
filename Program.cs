71fad9f1d73ff671081a9484bbf08fb6abec542d
Pedro Andreotti <pedroandreottijetforce@gmail.com>
2023-05-07 20:26:35 -0300

    Eh isso ae

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-templ
+int dividendo, divisor;
+Console.WriteLine("Escreva um número");
+dividendo = Convert.ToInt32
+        (Console.ReadLine());
+
+Console.WriteLine("Escreva o possível multiplo.");
+divisor = Convert.ToInt32
+        (Console.ReadLine());
+
+
+Console.WriteLine("Analisando...");
+    if (divisor / dividendo == 0);
+{
+Console.WriteLine($"{divisor} não é múltiplo de {dividendo}.");
+}
+
+
+     else 
+{
+    Console.WriteLine(" não é divisor");
+}
+
+//Não consigo entender o porquê do meu programa dar erro na 16º linha,
+// tentei diversas vezes arrumar, mas eu simplesmente n consigo
+//Não tenho idéia do que eu poderia colocar no lugar do else, ou desse colchete...
